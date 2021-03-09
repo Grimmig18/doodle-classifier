112eedc2cd1c0e513c97e2b5fbf045c6e1356768
Benno Grimm <43752927+Grimmig18@users.noreply.github.com>
2021-03-09 22:27:48 +0100

    Move large files to LFS

--- src/data_loader.py
+++ src/data_loader.py
@@ -5,9 +5,6 @@ sys.path.append(root_dir)
 
 import json
 import numpy as np
-from src.dict_obj import DictObject as DO
-
-
 class DataLoader:
     
     # Indexes of default tree image words
@@ -20,31 +17,42 @@ class DataLoader:
     DRAWING = 'drawing'
 
 
-    def __init__(self, files=['helicopter.ndjson', 'octopus.ndjson', 'pizza.ndjson'], path='../data/processed/', default_width=256, default_height=256):
+    def __init__(self, files=['helicopter.ndjson', 'octopus.ndjson', 'pizza.ndjson'], path='../data/processed/', width=256, height=256):
         self.files = files
         self.path = path
 
-        self.default_width = default_width
-        self.default_height = default_height
+        self.width = width
+        self.height = height
 
 
-    def load_data_from_file(self, file_index, lines=0):
+    def load_data_from_file(self, file_index, line=0):
         """
         Loads the JSON from one of the files from self.files by index.
         Returns data as dict object
         """
         with open(self.path + self.files[file_index], 'r') as f:
-            return [eval(x) for i, x in enumerate(f) if i in lines]
+            counter = 0
+            for l in f:
+                if counter == line:
+                    return eval(l)
+                counter = counter + 1
 
+
+    def drawing_array_to_tupels(self, drawing):
+        tupels = []
+        for i in range(len(drawing)):
+            tupels.extend([(drawing[i][0][j], drawing[i][1][j]) for j in range(len(drawing[i][0]))])
+
+        return tupels
 
 
     def matrix_from_image(self, points):
         """
-        Returns a default_width * default_height matrix filled with 0s.
+        Returns a width * height matrix filled with 0s.
         The (x, y) tupels in the points array define black pixels.
         """
 
-        mat = np.full((self.default_width, self.default_height), 0)
+        mat = np.full((self.width, self.height), 0)
         for point in points:
             mat[point[0], point[1]] = 1
 
--- src/preprocess_data.py
+++ src/preprocess_data.py
@@ -1,29 +1,53 @@
 import json
 import ndjson
+import numpy as np
+
+f_name = "helicopter.ndjson"
+
+def find_local_max_in_drawing(drawing): 
+    max = 0
+    for stroke in drawing:
+        for point in range(len(stroke[0])):
+            if stroke[0][point] > max:
+                max = stroke[0][point]
+            if stroke[1][point] > max:
+                max = stroke[1][point] 
+    return max
 
 # Create dict for processed data
 rec = []
 
 # Read raw data
-with open('./data/raw/octopus.ndjson', 'r', encoding='utf-8') as raws:
+with open('./data/raw/' + f_name, 'r', encoding='utf-8') as raws:
     reader = ndjson.reader(raws)
     total_images = 0
+
+    highest_x = 0
+    highest_y = 0
 
     for raw in raws:
         raw_json = json.loads(raw)
         
         if raw_json['recognized'] == True:
-            # Extrect only the information we care about
+            # Extract only the information we care about
             new_r = {
                 'word': raw_json['word'],
                 'drawing': raw_json['drawing']
             }
+            
+
+            # Find highest Value
+            max = find_local_max_in_drawing(raw_json['drawing'])
+
+            # Scale strokes
+            for stroke in raw_json['drawing']:
+                stroke[0] = [int(x) for x in np.rint(np.interp(stroke[0], (0, max), (0, 256))).tolist()]
+                stroke[1] = [int(x) for x in np.rint(np.interp(stroke[1], (0, max), (0, 256))).tolist()]
+            
             rec.append(new_r)
 
 
-
-
-with open('./data/processed/octopus.ndjson', 'w', encoding='utf-8') as f:
+with open('./data/processed/' + f_name, 'w', encoding='utf-8') as f:
     for r in rec:
         f.write(str(r)+ '\n')
     
